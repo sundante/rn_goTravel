e8083e4f7878ae8d237d7c11d987f87f1e53644c
Suryaprakash Singh <suryaprakashsingh@Suryaprakashs-MacBook-Air.local>
2023-03-12 12:04:54 +0530

    Final Commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import { Text, SafeAreaView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen, Discover } from "./screens";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Discover" component={Discover} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
